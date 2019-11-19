a54753f766b61cd7cea3d0aedc3b03d309ca2238
majasveljo <majasveljo@gmail.com>
2019-11-19 01:37:32 +0100

    some minor data added.

--- monsters-rolodex/src/components/card-list/card-list.component.js
+++ monsters-rolodex/src/components/card-list/card-list.component.js
@@ -7,8 +7,7 @@ import './card-list.style.css';
 export const CardList = (props) => {
     return <div className="card-list">
     {props.monsters.map(monster =>
-            <Card key={monster.id} monster={monster} />
-        )
-    }
+        <Card key={monster.id} monster={monster} />
+    )}
     </div>
 }
--- monsters-rolodex/src/components/card/card.component.js
+++ monsters-rolodex/src/components/card/card.component.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import "./card.styles.css";
+
 export const Card = (props) => (
-    <div>
-        <h1>{props.monster.name}</h1>
+    <div className="card-container">
+        <img
+            alt="monster"
+            src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
+        />
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
     </div>
 )
