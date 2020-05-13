393368f230fc893f6d9eacd3846cc996c4289dfb
dragan1992peric <dragan1992peric@gmail.com>
2020-05-13 08:37:21 +0200

    saga in progress

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,18 +2,35 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { googleSignInSuccess, googleSignInFaliure } from './user.actions';
+import { signInSuccess, signInFaliure } from './user.actions';
 
 import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+export function* getSnapshotFromUserAuth (userAuth) {
+    try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+        const userSnapshot = yield userRef.get();
+        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data }))
+    } catch (error) {
+        yield put(signInFaliure(error));
+    }
+}
 
 export function* signInWithGoogle() {
     try {
         const {user} = yield auth.signInWithPopup(googleProvider);
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapshot = yield userRef.get();
-        yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data }))
+        yield getSnapshotFromUserAuth(user);
     } catch (error) {
-        yield put(googleSignInFaliure(error));
+        yield put(signInFaliure(error));
+    }
+}
+
+export function* signInWithEmail({payload: { email, password }}) {
+    try {
+        const  { user } = yield auth.signInWithEmailAndPassword(email, password);
+        yield getSnapshotFromUserAuth(user);
+    } catch(error) {
+        put(signInFaliure(error))
     }
 }
 
@@ -21,6 +38,13 @@ export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
 }
 
+export function* onEmailSignInStart() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
+}
+
 export function* userSagas() {
-    yield all([call(onGoogleSignInStart)])
+    yield all([
+        call(onGoogleSignInStart),
+        call(onEmailSignInStart),
+    ])
 }
